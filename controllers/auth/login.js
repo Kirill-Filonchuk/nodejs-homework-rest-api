e4c5753057541b797c4bba56b45257604cc01658
Kirill Filonchuk <kir.realtor@gmail.com>
2023-03-08 21:12:13 +0200

    finish - no docker

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -6,6 +6,7 @@ const { HttpError } = require("../../helpers");
 
 const login = async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
 
   // Проверка пользователя - Подтвердил (verify - true?) ли он свою Почту
@@ -13,7 +14,7 @@ const login = async (req, res) => {
     throw HttpError(401, "Email is not verify");
   }
 
-  console.log(user.comparePassword(password));
+  // console.log(user.comparePassword(password));
   if (!user || !user.comparePassword(password)) {
     throw HttpError(401, "Unauthorized. Email or password is wrong");
   }
@@ -31,8 +32,10 @@ const login = async (req, res) => {
   };
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "8h" });
+
   await User.findByIdAndUpdate(user._id, { token });
-  res.json({
+
+  res.status(200).json({
     Status: 200,
     ResponseBody: {
       token: token,
